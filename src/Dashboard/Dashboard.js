b976d036761f2632123f4183a7272550720cc8ae
MikeMorris101 <mikemorris@ymail.com>
2020-08-24 21:30:23 +0530

    Add cards

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Brand } from './Brand';
+import { Container } from './Container';
+import { GlobalStyle } from './GlobalStyle';
+import { Card } from './Card';
+
+export function Dashboard() {
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Brand />
+        <Card height={400}>Chart goes here</Card>
+        <Card height={400}>table goes here</Card>
+      </Container>
+    </>
+  );
+}
