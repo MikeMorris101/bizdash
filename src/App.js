269e6cd586360297fc72fd650c6dc3b19860fd08
MikeMorris101 <mikemorris@ymail.com>
2020-08-24 21:10:37 +0530

    dashboard brand container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import { Dashboard } from './Dashboard/Dashboard';
 function App() {
-  return <div>Hello world</div>;
+  return <Dashboard></Dashboard>;
 }
 
 export default App;
