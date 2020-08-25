965f8aa00576bacd84ed3b6874ecd3877029c26b
MikeMorris101 <mikemorris@ymail.com>
2020-08-25 22:08:23 +0530

    add theme context and dark switch

--- src/Dashboard/GlobalStyle.js
+++ src/Dashboard/GlobalStyle.js
@@ -1,13 +1,18 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
+export const black = '#252020';
 export const darkGrey = '#515263';
 export const mediumGrey = '#dad9d9';
 export const lightGrey = '#f7f7f7';
+export const brandColor = '#CAF0F8';
 
 export const GlobalStyle = createGlobalStyle`
     body{
+        h1{
+            ${({ dark }) => dark && `color:${brandColor}`};
+        }
         color:${darkGrey};
-        background-color:${lightGrey};
+        background-color:${({ dark }) => (dark ? black : lightGrey)};
         font-family: 'Blinker', sans-serif;
     }
 `;
